434274409bd1932fd88b3e006b60b58fd103bc01
jiwen624 <jiwen624@gmail.com>
2021-11-09 15:46:37 -0800

    new bootstrap packages provider

--- custom/src/main/java/com/appoptics/opentelemetry/extensions/AppOpticsBootstrapPackagesProvider.java
+++ custom/src/main/java/com/appoptics/opentelemetry/extensions/AppOpticsBootstrapPackagesProvider.java
@@ -1,16 +1,20 @@
 package com.appoptics.opentelemetry.extensions;
 
 import com.google.auto.service.AutoService;
-import io.opentelemetry.javaagent.spi.BootstrapPackagesProvider;
+import io.opentelemetry.instrumentation.api.config.Config;
+import io.opentelemetry.javaagent.extension.bootstrap.BootstrapPackagesBuilder;
+import io.opentelemetry.javaagent.extension.bootstrap.BootstrapPackagesConfigurer;
 
-import java.util.Arrays;
-import java.util.List;
-
-@AutoService(BootstrapPackagesProvider.class)
-public class AppOpticsBootstrapPackagesProvider implements BootstrapPackagesProvider {
-
+/**
+ * This adds the Joboe core classes to the list which would always be loaded by the bootstrap classloader,
+ * no matter what classloader is used initially.
+ * The Otel agent instruments all classloaders and checks the class named to be loaded. It will load the class
+ * with the bootstrap classloader if the class mateches any of the prefix in the list above.
+ */
+@AutoService(BootstrapPackagesConfigurer.class)
+public class AppOpticsBootstrapPackagesProvider implements BootstrapPackagesConfigurer {
     @Override
-    public List<String> getPackagePrefixes() {
-        return Arrays.asList("com.tracelytics");
+    public void configure(Config config, BootstrapPackagesBuilder builder) {
+        builder.add("com.tracelytics");
     }
 }
