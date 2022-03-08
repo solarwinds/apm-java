b7f589ac1d7240bc66c191b6ba4f5d4237599b86
jiwen624 <jiwen624@gmail.com>
2022-03-08 14:58:37 -0800

    initial commit

---
+++ benchmark/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+  id("java")
+  id("com.diffplug.spotless") version "6.1.2"
+}
+
+spotless {
+  java {
+    googleJavaFormat()
+    licenseHeaderFile(rootProject.file("../buildscripts/spotless.license.java"), "(package|import|public)")
+    target("src/**/*.java")
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation("org.testcontainers:testcontainers:1.16.2")
+  testImplementation("org.testcontainers:postgresql:1.15.3")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+  testImplementation("com.squareup.okhttp3:okhttp:4.9.1")
+  testImplementation("org.jooq:joox:1.6.2")
+  testImplementation("com.jayway.jsonpath:json-path:2.6.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+}
+
+tasks {
+  test {
+    useJUnitPlatform()
+  }
+}
