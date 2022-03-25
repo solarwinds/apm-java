40b0e3e950a7783dbdcbd56385df83fc9f71e104
jiwen624 <jiwen624@gmail.com>
2022-03-25 16:03:31 -0700

    report query args

--- instrumentation/jdbc/src/main/java/com/appoptics/opentelemetry/instrumentation/PreparedStatementInstrumentation.java
+++ instrumentation/jdbc/src/main/java/com/appoptics/opentelemetry/instrumentation/PreparedStatementInstrumentation.java
@@ -1,6 +1,11 @@
 package com.appoptics.opentelemetry.instrumentation;
 
+import com.tracelytics.joboe.XTraceOptions;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.context.Context;
+import io.opentelemetry.context.ContextKey;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
@@ -11,6 +16,10 @@ import net.bytebuddy.matcher.ElementMatcher;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.implementsInterface;
@@ -34,15 +43,21 @@ public class PreparedStatementInstrumentation implements TypeInstrumentation {
     public void transform(TypeTransformer transformer) {
         transformer.applyAdviceToMethod(
                 nameStartsWith("set").and(takesArguments(2)).and(isPublic()),
-                PreparedStatementInstrumentation.class.getName() + "$PreparedStatementAdvice");
+                PreparedStatementInstrumentation.class.getName() + "$PreparedStatementSetAdvice");
+
+        transformer.applyAdviceToMethod(
+                nameStartsWith("execute").and(takesArguments(0)).and(isPublic()),
+                PreparedStatementInstrumentation.class.getName() + "$PreparedStatementExecuteAdvice");
     }
 
     @SuppressWarnings("unused")
-    public static class PreparedStatementAdvice {
+    public static class PreparedStatementSetAdvice {
 
         @Advice.OnMethodEnter(suppress = Throwable.class)
         public static void onEnter(
                 @Advice.This PreparedStatement statement,
+                @Advice.Argument(value = 0, readOnly = false) int index,
+                @Advice.Argument(value = 1, readOnly = false) Object value,
                 @Advice.Local("otelCallDepth") CallDepth callDepth) {
             // Connection#getMetaData() may execute a Statement or PreparedStatement to retrieve DB info
             // this happens before the DB CLIENT span is started (and put in the current context), so this
@@ -55,7 +70,13 @@ public class PreparedStatementInstrumentation implements TypeInstrumentation {
             if (callDepth.getAndIncrement() > 0) {
                 return;
             }
-
+            Context context = currentContext();
+            SortedMap<String, String> queryArgs = context.get(QueryArgsContextKey.KEY);
+            if (queryArgs == null) {
+                queryArgs = new TreeMap<>();
+                context.with(QueryArgsContextKey.KEY, queryArgs).makeCurrent();
+            }
+            queryArgs.put(String.valueOf(index), JdbcEventValueConverter.convert(value).toString()); // TODO
         }
 
         @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
@@ -66,6 +87,35 @@ public class PreparedStatementInstrumentation implements TypeInstrumentation {
                 return;
             }
 
+        }
+    }
+    public static class QueryArgsContextKey {
+        public static final ContextKey<SortedMap<String, String>> KEY = ContextKey.named("query-args-key");
+        private QueryArgsContextKey() {}
+    }
+
+    public static class QueryArgsAttributeKey {
+        public static final AttributeKey<List<String>> KEY = AttributeKey.stringArrayKey("QueryArgs");
+    }
+    public static class PreparedStatementExecuteAdvice {
+        @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
+        public static void stopSpan(
+                @Advice.Thrown Throwable throwable,
+                @Advice.Local("otelCallDepth") CallDepth callDepth) {
+            if (callDepth.decrementAndGet() > 0) {
+                return;
+            }
+            Context context = currentContext();
+            Span span = Span.fromContext(context);
+            SpanContext spanContext = span.getSpanContext();
+            if (!(spanContext.isValid() && spanContext.isSampled())) {
+                SortedMap<String, String> argsMap = context.get(QueryArgsContextKey.KEY);
+
+                if (argsMap != null) {
+                    List<String> queryArgs = new ArrayList<>(argsMap.values());
+                    span.setAttribute(QueryArgsAttributeKey.KEY, queryArgs);
+                }
+            }
         }
     }
 }
