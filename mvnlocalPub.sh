637ea457029c1bd239cc12ea615b4e5edd7f4dac
CleverChuk <chukwubuikem.umeugwa@solarwinds.com>
2023-02-16 11:15:04 -0500

    NH-20004: Java agent: upgrade Otel dependency to the latest version - [JIRA](https://swicloud.atlassian.net/browse/NH-20004) - exclude SLF4J api classes from agent class loader path

--- mvnlocalPub.sh
+++ mvnlocalPub.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+# This script exist to enable conveniently building and publishing the artifacts to local maven repo
+./gradlew clean build
 ./gradlew agent:publishLocalInstallPublicationToMavenLocal
 ./gradlew solarwinds-otel-sdk:publishLocalInstallPublicationToMavenLocal
 
