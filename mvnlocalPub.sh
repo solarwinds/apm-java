021986a9c983b3ac95001a007419bd306a6525ab
CleverChuk <chukwubuikem.umeugwa@solarwinds.com>
2023-02-22 08:59:33 -0500

    NH-33952: Release Java Agent 0.15.0 - [JIRA](https://swicloud.atlassian.net/browse/NH-33952) - use a single publication - exclude local publishing from signing

--- mvnlocalPub.sh
+++ mvnlocalPub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 # This script exist to enable conveniently building and publishing the artifacts to local maven repo
 ./gradlew clean build
-./gradlew agent:publishLocalInstallPublicationToMavenLocal
-./gradlew solarwinds-otel-sdk:publishLocalInstallPublicationToMavenLocal
+./gradlew agent:publishToMavenLocal
+./gradlew solarwinds-otel-sdk:publishToMavenLocal
 
