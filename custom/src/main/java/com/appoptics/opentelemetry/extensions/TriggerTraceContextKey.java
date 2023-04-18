2e378cd6f14418d1069f250ba55f042d67988bf5
CleverChuk <chukwubuikem.umeugwa@solarwinds.com>
2023-04-18 15:29:04 -0400

    NH-12169: add unit test, AppOpticsContextPropagatorTest.java, that tests context propagation logic implemented by our custom propagator.

--- custom/src/main/java/com/appoptics/opentelemetry/extensions/TriggerTraceContextKey.java
+++ custom/src/main/java/com/appoptics/opentelemetry/extensions/TriggerTraceContextKey.java
@@ -7,9 +7,9 @@ import javax.annotation.concurrent.Immutable;
 
 @Immutable
 final class TriggerTraceContextKey {
-    static final ContextKey<XTraceOptions> KEY = ContextKey.named("sw-trigger-trace-key");
-    static final ContextKey<String> XTRACE_OPTIONS = ContextKey.named("xtrace-options");
-    static final ContextKey<String> XTRACE_OPTIONS_SIGNATURE = ContextKey.named("xtrace-options-signature");
+    public static final ContextKey<XTraceOptions> KEY = ContextKey.named("sw-trigger-trace-key");
+    public static final ContextKey<String> XTRACE_OPTIONS = ContextKey.named("xtrace-options");
+    public static final ContextKey<String> XTRACE_OPTIONS_SIGNATURE = ContextKey.named("xtrace-options-signature");
 
     private TriggerTraceContextKey() { }
 }
