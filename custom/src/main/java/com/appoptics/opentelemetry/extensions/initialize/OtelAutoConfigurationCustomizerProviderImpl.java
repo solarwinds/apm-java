59b6b3b98001a0c882af452c7cc4029acd4608b8
cleverchuk <chubiyke@gmail.com>
2023-10-24 09:28:33 -0400

    NH-64022: - remove `OtlpComponentPropertiesCustomizer` because SDK takes care of this concern

--- custom/src/main/java/com/appoptics/opentelemetry/extensions/initialize/OtelAutoConfigurationCustomizerProviderImpl.java
+++ custom/src/main/java/com/appoptics/opentelemetry/extensions/initialize/OtelAutoConfigurationCustomizerProviderImpl.java
@@ -3,7 +3,6 @@ package com.appoptics.opentelemetry.extensions.initialize;
 import com.appoptics.opentelemetry.extensions.AppOpticsPropertiesSupplier;
 import com.appoptics.opentelemetry.extensions.AppOpticsTracerProviderCustomizer;
 import com.appoptics.opentelemetry.extensions.lambda.LambdaRuntimeTraceProviderCustomizer;
-import com.appoptics.opentelemetry.extensions.lambda.OtlpComponentPropertiesCustomizer;
 import com.google.auto.service.AutoService;
 import com.tracelytics.joboe.config.InvalidConfigException;
 import com.tracelytics.logging.Logger;
@@ -53,8 +52,7 @@ public class OtelAutoConfigurationCustomizerProviderImpl implements AutoConfigur
         autoConfiguration
                 .addPropertiesSupplier(new AppOpticsPropertiesSupplier())
                 .addTracerProviderCustomizer(new LambdaRuntimeTraceProviderCustomizer(new AppOpticsTracerProviderCustomizer()))
-                .addResourceCustomizer(new AutoConfiguredResourceCustomizer())
-                .addPropertiesCustomizer(new OtlpComponentPropertiesCustomizer());
+                .addResourceCustomizer(new AutoConfiguredResourceCustomizer());
     }
 
     @Override
