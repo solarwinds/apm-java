a9fee6cd28349e3e023c7f431da53420b49cb35d
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2023-11-13 09:17:01 -0500

    NH-64022: add `sw.transaction` to root span attribute

--- custom/src/main/java/com/appoptics/opentelemetry/extensions/AppOpticsRootSpanProcessor.java
+++ custom/src/main/java/com/appoptics/opentelemetry/extensions/AppOpticsRootSpanProcessor.java
@@ -1,41 +1,51 @@
 package com.appoptics.opentelemetry.extensions;
 
 import com.appoptics.opentelemetry.core.RootSpan;
+import com.tracelytics.logging.Logger;
+import com.tracelytics.logging.LoggerFactory;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
+
 import javax.annotation.Nonnull;
 
-/** Span processor to keep track of the root span of a trace */
+/**
+ * Span processor to keep track of the root span of a trace
+ */
 public class AppOpticsRootSpanProcessor implements SpanProcessor {
-  @Override
-  public void onStart(@Nonnull Context parentContext, @Nonnull ReadWriteSpan span) {
-    SpanContext parentSpanContext = Span.fromContext(parentContext).getSpanContext();
-    if (!parentSpanContext.isValid()
-        || parentSpanContext.isRemote()) { // then a root span of this service
-      RootSpan.setRootSpan(span);
+
+    private static final Logger logger = LoggerFactory.getLogger();
+
+    @Override
+    public void onStart(@Nonnull Context parentContext, @Nonnull ReadWriteSpan span) {
+        SpanContext parentSpanContext = Span.fromContext(parentContext).getSpanContext();
+        if (!parentSpanContext.isValid() || parentSpanContext.isRemote()) { //then a root span of this service
+            RootSpan.setRootSpan(span);
+            String transactionName = TransactionNameManager.getTransactionName(span.toSpanData());
+
+            span.setAttribute("sw.transaction", transactionName);
+            logger.debug(String.format("Transaction name derived on root span start: %s", transactionName));
+        }
     }
-  }
 
-  @Override
-  public boolean isStartRequired() {
-    return true;
-  }
-
-  @Override
-  public void onEnd(ReadableSpan span) {
-    SpanContext parentSpanContext = span.toSpanData().getParentSpanContext();
-    if (!parentSpanContext.isValid()
-        || parentSpanContext.isRemote()) { // then a root span of this service
-      RootSpan.clearRootSpan(span.getSpanContext().getTraceId());
+    @Override
+    public boolean isStartRequired() {
+        return true;
     }
-  }
 
-  @Override
-  public boolean isEndRequired() {
-    return true;
-  }
+    @Override
+    public void onEnd(ReadableSpan span) {
+        SpanContext parentSpanContext = span.toSpanData().getParentSpanContext();
+        if (!parentSpanContext.isValid() || parentSpanContext.isRemote()) { //then a root span of this service
+            RootSpan.clearRootSpan(span.getSpanContext().getTraceId());
+        }
+    }
+
+    @Override
+    public boolean isEndRequired() {
+        return true;
+    }
 }
