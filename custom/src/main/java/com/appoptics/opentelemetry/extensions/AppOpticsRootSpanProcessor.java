c2fd8ac245a2f9f9c2e626b4b07a552744baf87d
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2023-11-13 09:17:01 -0500

    NH-64022: add `sw.transaction` to root span attribute

--- custom/src/main/java/com/appoptics/opentelemetry/extensions/AppOpticsRootSpanProcessor.java
+++ custom/src/main/java/com/appoptics/opentelemetry/extensions/AppOpticsRootSpanProcessor.java
@@ -2,6 +2,9 @@ package com.appoptics.opentelemetry.extensions;
 
 import com.appoptics.opentelemetry.core.RootSpan;
 import com.solarwinds.joboe.shaded.javax.annotation.Nonnull;
+import com.solarwinds.shaded.javax.annotation.Nonnull;
+import com.solarwinds.joboe.core.logging.Logger;
+import com.solarwinds.joboe.core.logging.LoggerFactory;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.context.Context;
@@ -11,14 +14,20 @@ import io.opentelemetry.sdk.trace.SpanProcessor;
 
 /** Span processor to keep track of the root span of a trace */
 public class AppOpticsRootSpanProcessor implements SpanProcessor {
-  @Override
-  public void onStart(@Nonnull Context parentContext, @Nonnull ReadWriteSpan span) {
-    SpanContext parentSpanContext = Span.fromContext(parentContext).getSpanContext();
-    if (!parentSpanContext.isValid()
-        || parentSpanContext.isRemote()) { // then a root span of this service
-      RootSpan.setRootSpan(span);
+
+    private static final Logger logger = LoggerFactory.getLogger();
+
+    @Override
+    public void onStart(@Nonnull Context parentContext, @Nonnull ReadWriteSpan span) {
+        SpanContext parentSpanContext = Span.fromContext(parentContext).getSpanContext();
+        if (!parentSpanContext.isValid() || parentSpanContext.isRemote()) { //then a root span of this service
+            RootSpan.setRootSpan(span);
+            String transactionName = TransactionNameManager.getTransactionName(span.toSpanData());
+
+            span.setAttribute("sw.transaction", transactionName);
+            logger.debug(String.format("Transaction name derived on root span start: %s", transactionName));
+        }
     }
-  }
 
   @Override
   public boolean isStartRequired() {
