0e008a4d59434293750a4246f98e544911c631af
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2023-11-15 13:06:15 -0500

    NH-64022: rebase and fix issues

--- custom/src/test/java/com/appoptics/opentelemetry/extensions/AppOpticsRootSpanProcessorTest.java
+++ custom/src/test/java/com/appoptics/opentelemetry/extensions/AppOpticsRootSpanProcessorTest.java
@@ -2,13 +2,12 @@ package com.appoptics.opentelemetry.extensions;
 
 import com.appoptics.opentelemetry.core.RootSpan;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.testing.trace.TestSpanData;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.data.StatusData;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.opentelemetry.semconv.SemanticAttributes;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -16,17 +15,12 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -51,7 +45,7 @@ class AppOpticsRootSpanProcessorTest {
                 .setEndEpochNanos(10000)
                 .setHasEnded(true)
                 .setStatus(StatusData.ok())
-                .setAttributes(Attributes.of(SemanticAttributes.HTTP_METHOD, "get"))
+                .setAttributes(Attributes.of(SemanticAttributes.HTTP_REQUEST_METHOD, "get"))
                 .build();
 
         MockedStatic<RootSpan> rootSpanMock = mockStatic(RootSpan.class);
