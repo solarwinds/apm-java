cff62225aef8da0de5a6db08fa17eb17bab803ca
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2023-11-16 10:18:00 -0500

    NH-64719: add environment variable sourced transaction name

--- custom/src/main/java/com/appoptics/opentelemetry/extensions/transaction/DefaultNamingScheme.java
+++ custom/src/main/java/com/appoptics/opentelemetry/extensions/transaction/DefaultNamingScheme.java
@@ -1,14 +1,16 @@
 package com.appoptics.opentelemetry.extensions.transaction;
 
+import com.tracelytics.joboe.config.ConfigManager;
+import com.tracelytics.joboe.config.ConfigProperty;
 import io.opentelemetry.api.common.Attributes;
 
-public class DefaultNamingScheme extends NamingScheme {
-  public DefaultNamingScheme(NamingScheme next) {
-    super(next);
-  }
+public class DefaultNamingScheme extends NamingScheme{
+    public DefaultNamingScheme(NamingScheme next) {
+        super(next);
+    }
 
-  @Override
-  public String createName(Attributes attributes) {
-    return null;
-  }
+    @Override
+    public String createName(Attributes attributes) {
+        return ConfigManager.getConfigOptional(ConfigProperty.AGENT_TRANSACTION_NAME, null);
+    }
 }
--- custom/src/test/java/com/appoptics/opentelemetry/extensions/transaction/DefaultNamingSchemeTest.java
+++ custom/src/test/java/com/appoptics/opentelemetry/extensions/transaction/DefaultNamingSchemeTest.java
@@ -1,10 +1,8 @@
 package com.appoptics.opentelemetry.extensions.transaction;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
+import com.tracelytics.joboe.config.ConfigManager;
+import com.tracelytics.joboe.config.ConfigProperty;
+import com.tracelytics.joboe.config.InvalidConfigException;
 import io.opentelemetry.api.common.Attributes;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,18 +10,35 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @ExtendWith(MockitoExtension.class)
 class DefaultNamingSchemeTest {
 
-  @InjectMocks private DefaultNamingScheme tested;
+    @InjectMocks
+    private DefaultNamingScheme tested;
 
-  @Mock private NamingScheme namingSchemeMock;
+    @Mock
+    private NamingScheme namingSchemeMock;
 
-  @Test
-  void verifyNoDelegationToNext() {
-    String name = tested.createName(Attributes.empty());
 
-    verify(namingSchemeMock, times(0)).createName(any());
-    assertNull(name);
-  }
+    @Test
+    void verifyNoDelegationToNext() {
+        String name = tested.createName(Attributes.empty());
+
+        verify(namingSchemeMock, times(0)).createName(any());
+        assertNull(name);
+    }
+
+    @Test
+    void verifyTransactionNameIsReturnedWhenSetInEnvironment() throws InvalidConfigException {
+        ConfigManager.setConfig(ConfigProperty.AGENT_TRANSACTION_NAME, "test");
+        String name = tested.createName(Attributes.empty());
+
+        assertEquals("test", name);
+    }
 }
