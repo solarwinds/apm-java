f48ee590fa125d9b597e227b6821e4d86eb7b646
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2024-08-20 12:48:24 -0400

    CUST-110209: do SDK maven snapshot release on push and use the snapshot SDK in smoke test. Add a new app test the sdk without the agent being attached

--- smoke-tests/netty-test-no-agent/build.gradle.kts
+++ smoke-tests/netty-test-no-agent/build.gradle.kts
@@ -19,7 +19,6 @@ import java.net.URI
 plugins {
     java
     application
-    id("de.undercouch.download") version "5.5.0"
 }
 
 group = "com.solarwinds"
@@ -48,23 +47,6 @@ tasks.test {
     useJUnitPlatform()
 }
 
-val swoAgentPath = project.buildDir.toString() + "/swo/solarwinds-apm-agent.jar"
-fun getAgentPath(downloadPath: String) = if (System.getenv("AGENT_PATH") == null) downloadPath
-else System.getenv("AGENT_PATH")
-
 application {
     mainClass.set("com.solarwinds.netty.NettyApp")
-    applicationDefaultJvmArgs = listOf("-javaagent:${getAgentPath(swoAgentPath)}")
-}
-
-tasks.register<de.undercouch.gradle.tasks.download.Download>("downloadSwoAgent") {
-    doNotTrackState("Runs everytime because new build needs to be downloaded")
-    src(System.getenv("AGENT_DOWNLOAD_URL"))
-    dest(swoAgentPath)
-    overwrite(true)
-}
-
-tasks.named("run", JavaExec::class) {
-    setEnvironment("SW_APM_SERVICE_KEY" to "${System.getenv("SW_APM_SERVICE_KEY")}:netty-pipeline-test-app")
-    dependsOn("downloadSwoAgent")
 }
