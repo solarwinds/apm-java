a18db14a59a5fd95767bf5e2768f9aa92ffa431f
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2024-12-23 16:28:28 -0500

    NH-94343: refactor shadow.gradle and instrumentation.gradle into conventions plugins

---
+++ buildSrc/src/main/kotlin/solarwinds.java-conventions.gradle.kts
@@ -0,0 +1,95 @@
+/*
+ * © SolarWinds Worldwide, LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  java
+  id ("com.github.jarmstrong.buildconfig")
+}
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+  maven {
+    url = uri("https://plugins.gradle.org/m2/")
+  }
+
+  maven {
+    name = "sonatype"
+    url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+  }
+
+  maven {
+    url = uri("https://maven.pkg.github.com/solarwinds/joboe")
+    credentials {
+      username = System.getenv("GITHUB_USERNAME")
+      password = System.getenv("GITHUB_TOKEN")
+    }
+  }
+}
+
+evaluationDependsOn(":dependencyManagement")
+val dependencyManagementConfiguration = configurations.create("dependencyManagement") {
+  isCanBeConsumed = false
+  isCanBeResolved = false
+  isVisible = false
+}
+
+afterEvaluate {
+  configurations.configureEach {
+    if (isCanBeResolved && !isCanBeConsumed) {
+      //propagate constraints
+      extendsFrom(dependencyManagementConfiguration)
+    }
+  }
+}
+
+val versions: Map<String, String> by rootProject.extra
+
+dependencies {
+  dependencyManagementConfiguration(platform(project(":dependencyManagement")))
+  testImplementation(project(":bootstrap"))
+
+  testImplementation("org.mockito:mockito-core")
+  testImplementation("org.mockito:mockito-inline")
+  testImplementation("org.mockito:mockito-junit-jupiter")
+
+  testImplementation("javax.annotation:javax.annotation-api")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+  testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure:${versions["opentelemetry"]}")
+  testImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-extension-api:${versions["opentelemetryJavaagentAlpha"]}")
+  testImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling:${versions["opentelemetryJavaagentAlpha"]}")
+
+  testImplementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api:${versions["opentelemetryJavaagent"]}")
+  testImplementation("io.opentelemetry.semconv:opentelemetry-semconv:${versions["opentelemetrySemconv"]}")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:${versions["opentelemetry"]}")
+
+  testImplementation("com.solarwinds.joboe:core:${versions["joboe"]}")
+  testImplementation("com.solarwinds.joboe:metrics:${versions["joboe"]}")
+  testImplementation("org.junit-pioneer:junit-pioneer")
+}
+
+tasks.withType<Test>().configureEach {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+  environment("SW_APM_SERVICE_KEY", "token:name")
+}
+
+
+
