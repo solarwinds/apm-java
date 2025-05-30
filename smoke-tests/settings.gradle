b0165b212176459288e98d6e1d81a0791f23875f
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2025-05-30 06:00:23 -0400

    NH-91926: migrate to central

--- smoke-tests/settings.gradle
+++ smoke-tests/settings.gradle
@@ -17,10 +17,6 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven {
-            name = "sonatype"
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-        }
     }
 }
 
