230ec80784cafbe22cfe69f1ca4632435eb8feb2
cleverchuk <chukwubuikem.umeugwa@solarwinds.com>
2025-12-10 11:56:14 -0500

    bump joboe to incorporate updated k8s namespace file path

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -7,7 +7,7 @@ val otelSdkVersion = "1.56.0"
 
 val mockitoVersion = "4.11.0"
 val byteBuddyVersion = "1.15.10"
-val joboeVersion = "10.0.25"
+val joboeVersion = "10.0.26"
 
 val opentelemetryJavaagentAlpha = "$otelAgentVersion-alpha"
 val opentelemetryAlpha = "$otelSdkVersion-alpha"
